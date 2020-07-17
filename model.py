a76d26330f6bb332f1bf121ca8794bfcf68a9afe
Chyld Medford <chyld.medford@gmail.com>
2020-07-17 01:28:18 -0700

    model

---
+++ model.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+d = load_iris()
+X = d.data
+y = d.target
+
+m = RandomForestClassifier().fit(X, y)
+pickle.dump(m, open('model.p', 'wb'))
